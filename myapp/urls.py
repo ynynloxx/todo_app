48d702a97c2e5c464c90c3add0bb3ad53a8ac1fb
yin-yin.lo <yin-yin.lo@rwth-aachen.de>
2019-09-27 15:43:46 +0200

    Setting up index.html and static files

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -6,4 +8,4 @@ app_name = 'myapp'
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
